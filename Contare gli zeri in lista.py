c9e5bff7ed2b9875fe6515daa23627adb9d6aad2
Shaxx129 <42065953+Shaxx129@users.noreply.github.com>
2020-11-04 21:03:18 +0100

    update

---
+++ Contare gli zeri in lista.py
@@ -0,0 +1,7 @@
+# ESERCIZIO CONTARE GLI ZERI NELLA LISTA
+def conta_zeri(lista):
+    c = 0                   #inizializzo il contatore
+    for n in l:         #per ogni n in lista
+        if n == 0:          #se n è = 0
+            c += 1          #aggiungo 1
+    return c
--- contiene uguali, consecutivi ecc.py
+++ contiene uguali, consecutivi ecc.py
@@ -1,21 +1,3 @@
-
-
-
-
-
-
-
-
-# ESERCIZIO CONTARE GLI ZERI NELLA LISTA
-def conta_zeri(lista):
-    c = 0                   #inizializzo il contatore
-    for n in l:         #per ogni n in lista
-        if n == 0:          #se n è = 0
-            c += 1          #aggiungo 1
-    return c
-
-
-
 def contiente_uguali_consecutivi(lista):
     for n in range(1,len(lista)):
         if lista[i] == lista[i-1]:
